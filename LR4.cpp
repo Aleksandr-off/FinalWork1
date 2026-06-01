f80df09a0e11924990049abb4adb656356e1b501
Dm <Dm@mail.ru>
2026-06-02 01:23:07 +0300

    Студент 2: изменил объявление переменных и добавил комментарии

---
+++ LR4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; //добавил переменную а
+	int b; //добавил переменную B
+    cout << "Vvedite chislo: ";
+    cin >> a; 
+    
+    // Проверка значения переменной a и вывод соответствующего сообщения
+    // Условная конструкция if-else if-else последовательно проверяет:
+    // сначала - положительное ли число, затем - отрицательное, и если ни то, ни другое - значит ноль
+    if (a > 0) {
+        cout << "Chislo polozhitelnoe" << endl;
+    } else if (a < 0) {
+        cout << "Chislo otricatelnoe" << endl;
+    } else {
+        cout << "Eto nol" << endl;
+    }
+    
+    return 0;
+}
